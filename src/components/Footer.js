e6647eb5a9c545cdfac6e4b48548772aa0f67b32
Angel Rivera <38502560+anmeth191@users.noreply.github.com>
2021-06-22 22:21:02 -0400

    Add files via upload
    today i was working how to update the schedule time if is sunday to friday it closes at 10 but if it is friday and saturday it closes at 11 , i got it working but it did not work the rendering with setting the state

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,59 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Footer = ()=>{
 
-    return(
+
+class Footer extends React.Component{
+ state = {
+     time:null,
+     days:['Sunday' ,'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
+     currentDay:'' ,
+     status:'Closed'
+ }
+
+//Function to display the day
+showDate = async ()=>{
+let catchDay = await new Date();
+let dayCatched = await catchDay.getDay();
+return this.state.days[dayCatched];
+}
+
+
+//this a function that returns a promise when the restaurant is open or closed
+showTime = async ()=>{
+let dateVariable = await new Date();
+let timeCatched = await dateVariable.getHours();
+return timeCatched;
+}//end of the showtime function
+
+showStatus = async (currentDay , time)=>{
+
+    let response = '';
+
+    if(currentDay === 'Tuesday' && time < 23){
+       response = await  'Open, Closes at 10:00 pm'
+    }if(currentDay === 'Wednesday' && time < 23){
+       response = await 'Open, Closes at 10:00 pm';
+    }if(currentDay ===  'Friday' && time < 24){
+     response = await 'Open Closes at 11:00pm'
+    }else{
+         response = await 'Closed';
+    }
+}
+
+componentDidMount(){
+
+this.showTime().then((response) =>{ this.setState({time:response})}).catch((error)=>{ console.log(error)})
+this.showDate().then((response)=>{ this.setState({currentDay:response})}).catch((error)=>{ console.log(error)})
+}//end of the component did mount
+
+componentDidUpdate(){
+    //this.showStatus(this.state.currentDay , this.state.time).then((response) =>{this.setState({status:response})}).catch((error)=>{console.log(error)})
+}
+    render()
+    {   
+       
+        return( 
         <div className="footer">
             <footer className="footer__foot">
                 <div className="footer__foot_links">
@@ -18,7 +68,11 @@ const Footer = ()=>{
                 </div>
 
                 <div className="footer__foot_hours">
-                     footer hours goes here 
+                     <div className="footer__foot_hours_container">
+                         <div className="footer__foot_hours_container-time">
+                             <h1>{this.state.status}</h1> 
+                          </div>
+                         </div>
                     </div>
                   
                   <div className="footer__foot_contact">
@@ -26,9 +80,9 @@ const Footer = ()=>{
                       </div>
             </footer>
             </div>
-    )
-}
-
+    )//end of the return
+}//end of the render 
+}//end of the class component 
 
 export default Footer;
 
